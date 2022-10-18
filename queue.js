1b5734ba7b782a8f585738a79d5866ca4c911ec2
thanhlucvip2 <doanthanhluc91bvh@gmail.com>
2022-10-18 15:58:11 +0700

    queue

--- queue.js
+++ queue.js
@@ -1,13 +1,28 @@
 function Queue() {
   this.store = [];
-  this.index = 0;
 
+  this.push = function (data) {
+    this.store.push(data);
+  };
+
+  this.Dequeue = function () {
+    return this.store.shift();
+  };
+
+  this.print = function () {
+    return this.store;
+  };
+
+  this.isEmpty = function () {
+    return this.store.length === 0;
+  };
 }
 
-
 /**
- * push 
- * Dequeue
- * print
- * isEmpty
+ * push : thêm phần tử vảo mảng
+ * Dequeue : xóa và in ra phần tử đẩu tiên được thêm
+ * print : in ra mảng
+ * isEmpty : check mảng rổng
  */
+
+const qu = new Queue();
